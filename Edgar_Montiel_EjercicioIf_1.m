80003b0c076a6edbde06d53b57a1b09efaddd05f
edgmontiel <43277709+edgmontiel@users.noreply.github.com>
2018-10-03 11:26:02 -0500

    Add files via upload
    Definir si el numero que se introdijo es par o impar

---
+++ Edgar_Montiel_EjercicioIf_1.m
@@ -0,0 +1,8 @@
+'Determinar si el numero es par o impar'
+prompt='introduce un numero: '
+A=input(prompt);
+    if rem(A,2)==0
+        fprintf('El numero es par')
+    else
+        fprintf('El numero es impar')
+    end
