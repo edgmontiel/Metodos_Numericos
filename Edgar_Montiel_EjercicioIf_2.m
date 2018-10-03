9337932a00e9cd7034049372f5a1327a39355131
edgmontiel <43277709+edgmontiel@users.noreply.github.com>
2018-10-03 11:29:12 -0500

    Edgar_Montiel_EjercicioIf_2
    Programa para definir si la calificación que se ingreso es aprobatoria o no.

---
+++ Edgar_Montiel_EjercicioIf_2.m
@@ -0,0 +1,8 @@
+'Verificar si la calificación es aprobatoria o reprobatoria'
+promt='Introduce la calificación: '
+calif=input(prompt);
+    if calif>=70
+        fprintf('Felicidades has aprobado')
+    else
+        fprintf('Continua con tus estudios, necesitas esforzarte mas')
+    end
