e1c8ab3bddf85a616a2d7d8ac4d608e420558318
edgmontiel <43277709+edgmontiel@users.noreply.github.com>
2018-10-03 11:32:35 -0500

    EjercicioIf_6
    Programa para calcular el costo del agua dependiendo de los litros que se consumieron.

---
+++ Edgar_Montiel_EjercicioIf_6.m
@@ -0,0 +1,16 @@
+prompt='Ingrese cuantos litros de agua consumio en el mes '
+L=input(prompt);
+    if  (L<51)
+        fprintf('el agua que consumio es gratis')
+    else
+        if (L>49)
+            if (L<201)
+            fprintf('el costo que pagarà por el consumo es de:')            
+            (L*3)
+            end
+        end
+        if (L>200)
+            fprintf('el costo que pagarà por el consumo es de:')            
+            (L*5)
+        end
+    end
