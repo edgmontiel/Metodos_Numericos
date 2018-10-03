147267511bd210ca187a499bad601b05dbd16d3e
edgmontiel <43277709+edgmontiel@users.noreply.github.com>
2018-10-03 11:31:51 -0500

    Ejercicio_If_4
    Programa para determinar cual de los tres numeros es el mayor.

---
+++ Edgar_Montiel_EjercicioIf_4.m
@@ -0,0 +1,22 @@
+prompt='Ingrese el primer numero '
+datoA=input(prompt);
+prompt='Ingrese el segundo numero '
+datoB=input(prompt);
+prompt='Ingrese el tercer numero '
+datoC=input(prompt);
+    if  (datoA>datoB)
+        if(datoA>datoC)
+        fprintf('El primer numero introducido es el mayor')
+        datoA
+        end
+    else
+        if (datoB>datoA)
+            if (datoB>datoC)
+            fprintf('El segundo numero introducido es el mayor')
+            datoB 
+    else
+            fprintf('El tercer numero introducido es el mayor')
+            datoC
+            end
+        end
+    end
