57679d55bae4f4d22c5064865bb1d6b5d8de07ae
edgmontiel <43277709+edgmontiel@users.noreply.github.com>
2018-10-03 22:20:22 -0500

    Update Edgar_Montiel_EjercicioIf_6.m

--- Edgar_Montiel_EjercicioIf_6.m
+++ Edgar_Montiel_EjercicioIf_6.m
@@ -1,16 +1,16 @@
 prompt='Ingrese cuantos litros de agua consumio en el mes '
 L=input(prompt);
-    if  (L<51)
-        fprintf('el agua que consumio es gratis')
-    else
         if (L>49)
             if (L<201)
-            fprintf('el costo que pagarà por el consumo es de:')            
-            (L*3)
+            fprintf('el costo que pagarâ€¡ por el consumo es de:')            
+            CARGO1=(L*3)
+            CARGOFIN1=(CARGO1-50)
             end
         end
         if (L>200)
-            fprintf('el costo que pagarà por el consumo es de:')            
-            (L*5)
+            fprintf('el costo que pagarâ€¡ por el consumo es de:')            
+            CARGO2=(L*5)
+            CARGOFIN2=(CARGO2-50)
+        else
+            fprintf('el costo que pagarâ€¡ por el consumo es de 150 :')
         end
-    end
